41437801fa48c77efd05bf52e8a337dbbf42771c
masjemblog <110954454+masjemblog@users.noreply.github.com>
2022-09-01 13:10:56 +0700

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const serverless = require("serverless-http");
-const processRequest = require("./app/process-request.js");
-const app = express();
-app.disable("x-powered-by");
-app.enable("trust proxy");
-app.all("*", processRequest);
-module.exports = app;
-module.exports.handler = serverless(app);
