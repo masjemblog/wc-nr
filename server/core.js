6dbc289c4c4c13d5f315305a9a6db35c74e5c4de
masjemblog <110954454+masjemblog@users.noreply.github.com>
2022-09-01 15:19:16 +0700

    wc

--- server/core.js
+++ server/core.js
@@ -485,7 +485,8 @@ app.use(async (req, res, next) => {
                   if (typePermalink == null) {
                     typePermalink = await randomPermalink(mapPermalink);
                   }
-                  let dom = (new JSDOM(body)).window.document;
+                  const cookieJar = new jsdom.CookieJar();
+                  let dom = new JSDOM(body, { cookieJar }).window.document;
                   await removeElement(dataSetting["element-remove"], dom);
                   await remakeUrlElement(dom, {
                     element: "link",
